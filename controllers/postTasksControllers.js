ff2354e7503867225b7aa46e530760ca27194113
Yuri Costa Matszuaki <yuri.mmatsuzaki@gmail.com>
2025-06-17 17:24:50 -0300

    Iniciando a rota post da API

--- controllers/postTasksControllers.js
+++ controllers/postTasksControllers.js
@@ -1,14 +1,12 @@
 const { pool } = require('../config/db');
 
-let title = 'Titulo 1';
-let task = 'Diga isso ao cara que me disse';
-let stay = 'in progress';
-
 async function postTasksControllers(req, res) {
     let connection; 
 
     try{
+        const {title, task ,stay} = req.body;
         connection = await pool.getConnection();
+        
         const [tasks] = await connection.execute('INSERT INTO tasks (idTask,title,task,stay) VALUES (?,?,?,?)', ["0", title,task,stay]);
 
         res.status(201).json({
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,10 @@
 const API_GET = 'http://localhost:8000/todolist/tasks';
-const API_PATH = 'http://localhost:8000/todolist/updatetask';
+const API_POST = 'http://localhost:8000/todolist/postTask';
 const buttonAdd = document.getElementById('buttonAdd');
 const taskContainer = document.getElementById('tasks');
+let titleTask;
+let textTask;
+let stayTask;
 
 async function displayTask() {
     const noneTask = document.getElementById('noneTask');
@@ -46,11 +49,63 @@ document.addEventListener('DOMContentLoaded', displayTask);
 
 buttonAdd.addEventListener('click', () => {
     const buttonSave = document.getElementById('buttonSave')
-
-    
+    const taskElement = document.createElement('div');
+    taskElement.classList.add('task');
 
     buttonAdd.style.display='none'
     buttonSave.style.display='block'
+
+        taskElement.innerHTML=`
+            <div class="bodyTask">
+                <h3 class="titleTask"><input type="text" id="inputTitle" placeholder="Insira um Título..."></h3>
+                <p class="textTask"><input type="text" id="inputTask" placeholder="Insira uma tarefa..."></p>
+            </div>
+            <div class="checkBoxTask">
+                <input type="checkbox" name="checkbox" id="checkbox" oninput="checkTask()">
+            </div>
+        `;
+        taskContainer.appendChild(taskElement);
+
+        const inputTitle = document.getElementById('inputTitle');
+        const inputTask = document.getElementById('inputTask');
+        const inputStay = document.getElementById('checkbox');
+
+        buttonSave.addEventListener('click', async () => {
+
+            if(inputTitle.value.length == 0 || inputTask.value.length == 0){
+                alert('Ainda há campos vazios!');
+            } else {
+                titleTask = inputTitle.value;
+                textTask = inputTask.value;
+                if(inputStay.checked == true){
+                    stayTask = 'finished';
+                } else {
+                    stayTask = 'in progress';
+                }
+
+                const textToJSON = {
+                    'title': titleTask,
+                    'task': textTask,
+                    'stay': stayTask
+                }
+
+                try{
+                    const response = await fetch(API_POST, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(textToJSON)});
+
+                    if(!response.ok){
+                        const errorData = await response.json();
+                        throw new Error(`Erro na API: ${response.status} - ${errorData.message || response.statusText}`);
+                    }
+                    buttonAdd.style.display='block';
+                    buttonSave.style.display='none';
+
+                } catch(error){
+
+                }
+
+            }
+        })
+
 })
 
 // function checkTask(){
